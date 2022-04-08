b1d568ee3c697725aaa48658ade04b76db958b2d
saputhebeast <sapumalwijekoon4@gmail.com>
2022-04-08 23:05:09 +0530

    #9

--- driving-school-login-check.php
+++ driving-school-login-check.php
@@ -1,13 +1,3 @@
 <?php 
-	
+	echo "<h1>Successfully Log In to the System!</h1>";
 ?>
-
-<!DOCTYPE HTML>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Successfully Log In to the System!</h1>
-</body>
-</html>
