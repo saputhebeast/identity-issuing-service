927ed3f094db93002d891f5310d9ec78bb15f3bc
saputhebeast <sapumalwijekoon4@gmail.com>
2022-04-09 16:46:10 +0530

    login page validation done using js

--- driving-school-login.php
+++ driving-school-login.php
@@ -13,10 +13,12 @@
 	<div class = "login-form">
 		<section class = "first-row">
 			<h1>Log In- Driving School Account</h1>
-			<form action = "driving-school-login-check.php" method = "POST">
-				<input type = "text" placeholder = "Enter the email address" name = "email"/><br>
-				<input type = "password" placeholder = "Enter the password" name = "password" id = "login-password"/><br>
-				<input type = "submit" value = "Log In" />
+			<form name = "login-form" action = "driving-school-login-check.php" onsubmit = "return validateForm();" method = "POST">
+					<input type = "text" placeholder = "Enter the email address" name = "email" id="login-email"/><br>
+					<p id="error-email"></p>
+					<input type = "password" placeholder = "Enter the password" name = "password" id = "login-password"/><br>
+					<p id="error-pass"></p>
+					<input type = "submit" value = "Log In"/>
 			</form>
 		</section>
 		
@@ -29,7 +31,7 @@
 			<p>Don't have an account? <a href = "driving-school-sign-up.php">Sign Up</a> here!</p>
 		</section>
 	</div>
-	
+
 	<script src = "scripts/driving-school-login.js"></script>
 </body>
 </html>
