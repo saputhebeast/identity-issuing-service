09ba8e7662c6f0100dca690ba0acf3b862bac8cb
saputhebeast <sapumalwijekoon4@gmail.com>
2022-04-10 00:34:33 +0530

    created basic structure of dashboard

--- driving-school-dashboard.php
+++ driving-school-dashboard.php
@@ -5,8 +5,50 @@
 <html>
 <head>
     <title>Driving School - Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" type = "text/css" href = "stylesheets/driving-school-dashboard.css"/>
 </head>
 <body>
+    <div class = "navigation">
+        <ul class = "nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Package Manage</a></li>
+            <li><a href="#">Registered User Manage</a></li>
+            <li><a href="#">Profile Settings</a></li>
+            <li style="float:right"><a href="#">Log Out</a></li>
+        </ul>
+    </div>
+    <div class = "container">
+        <div class="row">
+            <div class="column column-1">
+                <div class="content">
+                    <h1>Registered Users Count</h1>
+                    <h2>56</h2>
+                </div>
+            </div>
+            <div class="column column-2">
+                <div class="content">
+                    <h1>Listed Courses Count</h1>
+                    <h2>6</h2>
+                </div>
+            </div>
+            <div class="column column-3">
+                <div class="content">
+                    <h1>Registered Users Count</h1>
+                    <h2>56</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column column-4">
+            </div>
+            <div class="column column-5">
 
+            </div>
+            <div class="column column-6">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
