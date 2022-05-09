81c66d7695fbaaa5fb5155e9ccb63c9dc70b15c9
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-09 07:39:54 +0530

    added verification method to logout

--- php/driving-school-logout.php
+++ php/driving-school-logout.php
@@ -1,9 +1,13 @@
-<?php
-	session_start();
-	if(isset($_SESSION['username'])){
-        session_destroy();
-        header('Location: ../index.php');
-	}else{
-        header('Location: ./driving-school-dashboard.php');
+<?php 
+    session_start();
+    if(isset($_SESSION['username'])){
+        if(isset($_POST['btnOut'])){
+            session_destroy();
+            header("Location: ../index.php");
+        }else{
+            header("Location: ../php/driving-school-dashboard.php");
+        }
+    }else{
+        header("Location: ./driving-school-dashboard.php");
     }
 ?>
