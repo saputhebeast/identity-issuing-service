596628973ea2d1e32fb9265eb9ef2d2dfa6c59a9
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-10 18:48:01 +0530

    profile update function added update profile page #2

--- js/driving-school-profile-settings.js
+++ js/driving-school-profile-settings.js
@@ -116,3 +116,109 @@ function validatePassChangeForm(){
         return false;
     }
 }
+
+// https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function validateUpdate(){
+	let error = false;
+
+	let updateName = document.forms["update-account-form"]["school-name"].value;
+	let updateEmail = document.forms["update-account-form"]["email"].value;
+	let updatePhoneNumber = document.forms["update-account-form"]["phone-number"].value;
+	let updateAddress = document.forms["update-account-form"]["school_address"].value;
+	let updateDescription = document.forms["update-account-form"]["school_description"].value;
+
+	let changeName = document.getElementById("school-name");
+	let changeEmail = document.getElementById("email");
+	let changePhone = document.getElementById("phone-number");
+	let changeAddress = document.getElementById("school_address");
+	let changeDescription = document.getElementById("school_description");
+
+	let messageName = document.getElementById("error-name");
+	let messageEmail = document.getElementById("error-email");
+	let messagePhone = document.getElementById("error-phone");
+	let messageAddress = document.getElementById("error-address");
+	let messageDescription = document.getElementById("error-description");
+
+	// driving school name- update
+	if (updateName.length != 0) {
+		messageName.innerHTML = "";
+		changeName.style.border = "1px solid green";
+		messageName.style.color = "";
+	} else {
+		messageName.innerHTML = "Enter Driving School Name!";
+		changeName.style.border = "1px solid red";
+		messageName.style.color = "red";
+		error = true;
+	}
+	
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	// email validation
+	if(updateEmail.length == 0){
+		messageEmail.innerHTML = "Enter Email!";
+		changeEmail.style.border = "1px solid red";
+		messageEmail.style.color = "red";
+		error = true;
+	}else if(!String(updateEmail).toLocaleLowerCase().match(re)){
+		messageEmail.innerHTML = "Invalid Email!";
+		changeEmail.style.border = "1px solid red";
+		messageEmail.style.color = "red";
+		error = true;
+	}else{
+		messageEmail.innerHTML = "";
+		changeEmail.style.border = "1px solid green";
+		messageEmail.style.color = "";
+	}
+
+	// phone number check
+	if(updatePhoneNumber.length == 0){
+		messagePhone.innerHTML = "Enter Phone Number!";
+		changePhone.style.border = "1px solid red";
+		messagePhone.style.color = "red";
+		error = true;
+	}else if(updatePhoneNumber.length != 10){
+		messagePhone.innerHTML = "Invalid Phone Number!";
+		changePhone.style.border = "1px solid red";
+		messagePhone.style.color = "red";
+		error = true;
+	}else if(!isNumeric(updatePhoneNumber)){
+		messagePhone.innerHTML = "Please Enter Digits Only!";
+		changePhone.style.border = "1px solid red";
+		messagePhone.style.color = "red";
+		error = true;
+	}else{
+		messagePhone.innerHTML = "";
+		changePhone.style.border = "1px solid green";
+		messagePhone.style.color = ""
+	}
+
+	if(updateAddress.length == 0){
+		messageAddress.innerHTML = "Enter Phone Number!";
+		changeAddress.style.border = "1px solid red";
+		messageAddress.style.color = "red";
+		error = true;
+	}else{
+		messageAddress.innerHTML = "";
+		changeAddress.style.border = "1px solid green";
+		messageAddress.style.color = ""
+	}
+
+	if(updateDescription.length == 0){
+		messageDescription.innerHTML = "Enter Phone Number!";
+		changeDescription.style.border = "1px solid red";
+		messageDescription.style.color = "red";
+		error = true;
+	}else{
+		messageDescription.innerHTML = "";
+		changeDescription.style.border = "1px solid green";
+		messageDescription.style.color = ""
+	}
+
+	if(error){
+		return false;
+	}
+}
