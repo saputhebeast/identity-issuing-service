5775c825ddfce3adaab65a00edcab377e381c6c2
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-16 19:14:59 +0530

    added function to delete package image when delete the package

--- php/driving-school-package-manage-delete.php
+++ php/driving-school-package-manage-delete.php
@@ -6,8 +6,14 @@
         if(isset($_POST['btnDelete'])){
             $package_id = $_GET['package_id'];
             // echo $package_id;
-            $sql = "DELETE FROM package WHERE package_id = '$package_id'";
+            
+            $image = "SELECT image FROM Package WHERE package_id = '$package_id'";
+            $result = $conn->query($image);
+            $imageLink = $result->fetch_assoc();
+
+            $sql = "DELETE FROM Package WHERE package_id = '$package_id'";
             if($conn->query($sql)){
+                unlink($imageLink['image']);
                 echo "<script>alert('Package was deleted successfully!'); window.location = './driving-school-package-manage.php'</script>";
             }else{
                 echo "<script>alert('Package was not deleted successfully!'); window.location = './driving-school-package-manage.php'</script>";
