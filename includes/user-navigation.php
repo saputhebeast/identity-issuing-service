aa0b05ff8f6cc47a03c47b153a66e827906f3b34
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-21 10:45:38 +0530

    user navigation updated

--- includes/user-navigation.php
+++ includes/user-navigation.php
@@ -5,9 +5,9 @@
 <div class = "navigation">
     <ul class = "nav-links">
         <li><a href="#">Home</a></li>
-        <li><a href="#">Package Manage</a></li>
-        <li><a href="#">Registered User Manage</a></li>
-        <li><a href="#">Profile Settings</a></li>
+        <li><a href="#">New License</a></li>
+        <li><a href="#">Renew License</a></li>
+        <li><a href="../php/user-profile-settings.php">Profile Settings</a></li>
         <li style="float:right"><form action="../php/user-logout.php" method="POST"><input class="log-btn" type="submit" value="Log Out" name="btnOut"></form></li>
     </ul>
 </div>
