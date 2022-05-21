5f67e610ab6829ee6f407a25f0509e1f66a006bb
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-22 04:40:23 +0530

    completed user navigation

--- includes/user-navigation.php
+++ includes/user-navigation.php
@@ -4,9 +4,9 @@
 <link rel = "stylesheet" type = "text/css" href = "../css/driving-school-navigation.css"/>
 <div class = "navigation">
     <ul class = "nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">New License</a></li>
-        <li><a href="#">Renew License</a></li>
+        <li><a href="../php/user-dashboard.php">Home</a></li>
+        <li><a href="../php/user-new-license.php">New License</a></li>
+        <li><a href="../php/user-renew-license.php">Renew License</a></li>
         <li><a href="../php/user-profile-settings.php">Profile Settings</a></li>
         <li style="float:right"><form action="../php/user-logout.php" method="POST"><input class="log-btn" type="submit" value="Log Out" name="btnOut"></form></li>
     </ul>
