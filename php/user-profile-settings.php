98b1014cc32654d6fb6d566f264eff478d06fa45
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-22 22:02:22 +0530

    title updated

--- php/user-profile-settings.php
+++ php/user-profile-settings.php
@@ -12,7 +12,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Driving School - Profile Settings</title>
+    <title>User - Profile Settings</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel = "stylesheet" type = "text/css" href = "../css/driving-school-main.css"/>
--- php/user-renew-license.php
+++ php/user-renew-license.php
@@ -7,7 +7,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>User - New License</title>
+    <title>User - Renew License</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel = "stylesheet" type = "text/css" href = "../css/driving-school-dashboard.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
