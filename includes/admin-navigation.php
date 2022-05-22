a461fc99c40f1300dbb8916f17cee1235b48d1ec
saputhebeast <sapumalwijekoon4@gmail.com>
2022-05-22 17:13:33 +0530

    admin navigation updated

--- includes/admin-navigation.php
+++ includes/admin-navigation.php
@@ -4,8 +4,8 @@
 <link rel = "stylesheet" type = "text/css" href = "../css/driving-school-navigation.css"/>
 <div class = "navigation">
     <ul class = "nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Package Manage</a></li>
+        <li><a href="../php/admin-dashboard.php">Home</a></li>
+        <li><a href="../php/admin-license-manage.php">License Application Manage</a></li>
         <li><a href="#">Registered User Manage</a></li>
         <li><a href="#">Profile Settings</a></li>
         <li style="float:right"><form action="../php/admin-logout.php" method="POST"><input class="log-btn" type="submit" value="Log Out" name="btnOut"></form></li>
